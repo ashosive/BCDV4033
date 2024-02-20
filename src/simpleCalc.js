bdd7a96e3f6d9f8824313aaaeb0960c2e05d6b80
Ash Osive <65212969+ashosive@users.noreply.github.com>
2024-02-19 23:19:39 -0500

    Update simpleCalc.js

--- src/simpleCalc.js
+++ src/simpleCalc.js
@@ -1,17 +1,19 @@
 "use strict";
 
 var mathOperations = {
-sum: function(a,b) {
-    return a + b;
-},
-diff: function (a,b) {
-return a - b;
-},
-product: function (a,b) {
-return a * b;
-},
-divide: function (a,b) {
-return a / b;
-}
-}
-module.exports = mathOperations
+    sum: function(a,b) {
+        return a + b;
+    },
+    diff: function (a,b) {
+        return a - b;
+    },
+    product: function (a,b) {
+        return a * b;
+    },
+    // Add trailing comma to indicate the end of the object properties
+    divide: function (a,b) {
+        return a / b;
+    } // Missing trailing comma
+}; // Add semicolon to terminate the statement
+
+module.exports = mathOperations;
