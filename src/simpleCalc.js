c4ef74e9c0d352e2e0004131d2cc44b70996f453
Ash Osive <65212969+ashosive@users.noreply.github.com>
2024-02-19 23:19:55 -0500

    Update simpleCalc.js

--- src/simpleCalc.js
+++ src/simpleCalc.js
@@ -10,10 +10,9 @@ var mathOperations = {
     product: function (a,b) {
         return a * b;
     },
-    // Add trailing comma to indicate the end of the object properties
     divide: function (a,b) {
         return a / b;
-    } // Missing trailing comma
-}; // Add semicolon to terminate the statement
+    } 
+}; 
 
 module.exports = mathOperations;
